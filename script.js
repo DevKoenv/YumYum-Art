b9d8763ef7299868b907c64d85a220bf73955559
Gaming Mineblox <42892277+GamingMineblox@users.noreply.github.com>
2021-03-05 09:24:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var images = [
+    './images/background1.png',
+    './images/background2.gif',
+    './images/background3.gif',
+    './images/background4.gif',
+    './images/background5.png',
+]
+
+$('#container').append(
+    '<style>#container, .acceptContainer:before, .logoContainer:before {background: url(' +
+        images[Math.floor(Math.random() * images.length)] +
+        ') center fixed }'
+)
